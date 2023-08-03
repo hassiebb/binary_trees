db7180edc24be44f28d6c2cbaca6bcc31035e65f
Mohammad Hassieb <mohammadhassieb@gmail.com>
2023-08-03 15:29:33 +0300

    Update 7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_inorder - goes through a binary tree using in-order traversal
  * @tree: tree to traverse
- * @funcPo: pointer to a function to call for each node
+ * @funPo: pointer to a function to call for each node
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*funPo)(int))
 {
